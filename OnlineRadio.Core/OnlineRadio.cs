36b1e71bd54286c3f0a4bb9ea0aeda8f0bb5bf28
jackpoz <giacomopoz@gmail.com>
2012-08-10 21:23:32 +0200

    Initial commit.

---
+++ OnlineRadio.Core/OnlineRadio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OnlineRadio.Core
+{
+    public class OnlineRadio
+    {
+        string Url
+        {
+            get;
+            private set;
+        }
+
+        public OnlineRadio(string Url)
+        {
+            this.Url = Url;
+        }
+    }
+}
