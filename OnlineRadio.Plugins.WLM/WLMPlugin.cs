5fecd648f57419f8d2d3e54c4a94ade8158062f4
jackpoz <giacomopoz@gmail.com>
2012-08-24 17:03:48 +0200

    Add WLMPlugin project which will update WLM status with current song.

---
+++ OnlineRadio.Plugins.WLM/WLMPlugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OnlineRadio.Core;
+
+namespace OnlineRadio.Plugins.WLM
+{
+    public class WLMPlugin : IPlugin
+    {
+        string IPlugin.Name
+        {
+            get { return "WLMPlugin"; }
+        }
+
+        void IPlugin.OnCurrentSongChanged(object sender, CurrentSongEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
