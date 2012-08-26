f9a0182aae5e86c2a007b1782d53518509e5d0ca
jackpoz <giacomopoz@gmail.com>
2012-08-26 16:57:02 +0200

    Add LyricsPlugin project which will retrieve lyrics of current song.

---
+++ OnlineRadio.Plugins.Lyrics/LyricsPlugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OnlineRadio.Core;
+
+namespace OnlineRadio.Plugins.Lyrics
+{
+    public class LyricsPlugin : IPlugin
+    {
+        string IPlugin.Name
+        {
+            get { return "LyricsPlugin"; }
+        }
+
+        void IPlugin.OnCurrentSongChanged(object sender, CurrentSongEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
