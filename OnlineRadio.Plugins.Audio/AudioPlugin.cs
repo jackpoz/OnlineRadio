e30098cff28bd3c7e003d2cf3c5909ca036990bd
jackpoz <giacomopoz@gmail.com>
2012-11-01 13:10:39 +0100

    Add AudioPlugin project which will play current audio stream. Clean up build output when copying plugins.

---
+++ OnlineRadio.Plugins.Audio/AudioPlugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OnlineRadio.Core;
+
+namespace OnlineRadio.Plugins.Audio
+{
+    public class AudioPlugin : IPlugin
+    {
+        string IPlugin.Name
+        {
+            get { return "AudioPlugin"; }
+        }
+
+        void IPlugin.OnCurrentSongChanged(object sender, CurrentSongEventArgs args)
+        {}
+
+        void IPlugin.OnStreamUpdate(object sender, StreamUpdateEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
