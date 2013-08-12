0db1cd548ff1942c92db1c3c45ced7c4db3045db
jackpoz <giacomopoz@gmail.com>
2013-08-12 22:45:32 +0200

    Load lyrics from online services
    Load lyrics from online services, fill the "apikey" value in the OnlineRadio.Plugins.Lyrics app.config file to correctly load the lyrics. The plugin uses Newtonsoft.Json v5.0.6 to parse the json result.

--- OnlineRadio.Plugins.Lyrics/LyricsPlugin.xaml.cs
+++ OnlineRadio.Plugins.Lyrics/LyricsPlugin.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json.Linq;
 using OnlineRadio.Core;
 
 namespace OnlineRadio.Plugins.Lyrics
@@ -21,6 +26,9 @@ namespace OnlineRadio.Plugins.Lyrics
     /// </summary>
     public partial class LyricsPlugin : UserControl, IVisualPlugin
     {
+        const string lyricsUrl = "http://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track={0}&q_artist={1}";
+        readonly Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+
         UserControl IVisualPlugin.Control
         {
             get
@@ -34,9 +42,56 @@ namespace OnlineRadio.Plugins.Lyrics
             get { return "LyricsPlugin"; }
         }
 
+        string Lyrics
+        {
+            get
+            {
+                return _lyrics;
+            }
+            set
+            {
+                _lyrics = value;
+                Dispatcher.Invoke(() => LyricsText.Text = _lyrics);
+            }
+        }
+        string _lyrics;
+
+        string ApiKey
+        {
+            get
+            {
+                if (_apikey == null)
+                {
+                    Assembly currentAssembly = Assembly.GetCallingAssembly();
+                    Configuration currentConfiguration = ConfigurationManager.OpenExeConfiguration(currentAssembly.Location);
+                    _apikey = currentConfiguration.AppSettings.Settings["apikey"].Value;
+                }
+                return _apikey;
+            }
+        }
+        string _apikey;
+
         public void OnCurrentSongChanged(object sender, CurrentSongEventArgs args)
         {
-#warning To Do: load the lyrics and display them
+            Task.Run(() =>
+            {
+                try
+                {
+                    WebRequest request = GetRequest(string.Format(lyricsUrl, args.NewSong.Title, args.NewSong.Artist));
+                    WebResponse response = request.GetResponse();
+                    StreamReader responseStream = new StreamReader(response.GetResponseStream(), encode);
+                    JObject json = JObject.Parse(responseStream.ReadToEnd());
+                    if ((string)json["message"]["header"]["status_code"] == "401")
+                        Lyrics = "Unathorized call to Lyrics API, please check \"apikey\" parameter in the plugin config file."
+                                + "Current value: \"" + ApiKey + "\"";
+                    else
+                        Lyrics = (string)json["message"]["body"]["lyrics"]["lyrics_body"];
+                }
+                catch(ArgumentException)
+                {
+                    Lyrics = "Lyrics not found";
+                }
+            });
         }
 
         public void OnStreamUpdate(object sender, StreamUpdateEventArgs args)
@@ -46,6 +101,13 @@ namespace OnlineRadio.Plugins.Lyrics
         public LyricsPlugin()
         {
             InitializeComponent();
+        }
+
+        WebRequest GetRequest(string url)
+        {
+            url += "&apikey=" + ApiKey;
+            WebRequest request = WebRequest.Create(url);
+            return request;
         }
     }
 }
